90958403c4ec4268dae58280406ebf46b58d10de
Stack-Sculptor-Shanu <amarjeetjtpr@gmail.com>
2025-04-09 10:31:06 +0530

    removed rollup configuration

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // <--- ADD THIS LINE
+  base: './',
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@reduxjs/toolkit'],
-      output: {
-        globals: {
-          '@reduxjs/toolkit': 'ReduxToolkit',
-        },
-      },
+
     },
   },
 })
